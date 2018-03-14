4ccf1e2d5b4f6ee178de53e26014348ca49507f9
jakub.rzeplinski <jakub.rzeplinski@lingaro.com>
2018-03-14 11:17:44 +0100

    Add TwitterUserRepository.

---
+++ src/main/java/com/mgr/twitteranalyser/graph/repository/TwitterUserRepository.java
@@ -0,0 +1,13 @@
+package com.mgr.twitteranalyser.graph.repository;
+
+import java.io.Serializable;
+import com.mgr.twitteranalyser.graph.model.TwitterUser;
+import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TwitterUserRepository extends GraphRepository<TwitterUser>, Serializable {
+
+    TwitterUser findByScreenName(String screenName);
+
+}
