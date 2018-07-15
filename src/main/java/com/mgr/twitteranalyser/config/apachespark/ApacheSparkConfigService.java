6c355de2be9c6b03bb8e5da8a2fad97f01afa96e
jakub.rzeplinski <jakub.rzeplinski@lingarogroup.com>
2018-07-15 14:18:15 +0200

    Move Twitter credentials to application.properties.

--- src/main/java/com/mgr/twitteranalyser/config/apachespark/ApacheSparkConfigService.java
+++ src/main/java/com/mgr/twitteranalyser/config/apachespark/ApacheSparkConfigService.java
@@ -5,6 +5,7 @@ import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import twitter4j.Status;
 import twitter4j.auth.Authorization;
@@ -15,15 +16,19 @@ import twitter4j.conf.ConfigurationBuilder;
 @Service
 public class ApacheSparkConfigService {
 
-    //TODO move properties values to xml file
+    @Value("spring.social.twitter.application-name")
+    private String applicationName;
+    @Value("spring.social.twitter.consumer-key")
+    private String consumerKey;
+    @Value("spring.social.twitter.consumer-secret")
+    private String consumerSecret;
+    @Value("spring.social.twitter.access-token")
+    private String accessToken;
+    @Value("spring.social.twitter.access-token-secret")
+    private String accessTokenSecret;
+
     public TwitterCredentials getDefaultCredentials() {
-        return new TwitterCredentials(
-                "",
-                "",
-                "",
-                "",
-                ""
-        );
+        return new TwitterCredentials(applicationName, consumerKey, consumerSecret, accessToken, accessTokenSecret);
     }
 
     public JavaStreamingContext createContext(String applicationName) {
