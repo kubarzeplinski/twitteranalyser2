9f9046dd3f490903e2af8fc4b12138b12b794799
kubarzeplinski <kuba.rzeplinski@gmail.com>
2018-09-15 17:41:39 +0200

    Graph -> use js object instead of data from csv file to draw graph.

--- frontend/src/pages/graph/components/graph/Graph.js
+++ frontend/src/pages/graph/components/graph/Graph.js
@@ -4,99 +4,112 @@ import React from "react";
 import {Card} from "@blueprintjs/core";
 import * as d3 from "d3";
 
+const links = [
+    {source: "Sarah", target: "Alice"},
+    {source: "Eveie", target: "Alice"},
+    {source: "Peter", target: "Alice"},
+    {source: "Mario", target: "Alice"},
+    {source: "James", target: "Alice"},
+    {source: "Ted", target: "Alice"},
+    {source: "AAA", target: "Alice"},
+    {source: "P", target: "Alice"},
+    {source: "Marco", target: "Alice"},
+    {source: "James Rodrigues", target: "Alice"}
+];
+
 export default class Graph extends React.Component {
 
     componentDidMount() {
         // get the data
-        d3.csv("force.csv", function(error, links) {
-
-            var nodes = {};
+        var nodes = {};
 
 // Compute the distinct nodes from the links.
-            links.forEach(function(link) {
-                link.source = nodes[link.source] ||
-                    (nodes[link.source] = {name: link.source});
-                link.target = nodes[link.target] ||
-                    (nodes[link.target] = {name: link.target});
-                link.value = +link.value;
-            });
+        links.forEach(function (link) {
+            link.source = nodes[link.source] ||
+                (nodes[link.source] = {name: link.source});
+            link.target = nodes[link.target] ||
+                (nodes[link.target] = {name: link.target});
+            link.value = +link.value;
+        });
 
-            var width = 960,
-                height = 500;
+        var width = 960,
+            height = 500;
 
-            var force = d3.layout.force()
-                .nodes(d3.values(nodes))
-                .links(links)
-                .size([width, height])
-                .linkDistance(100)
-                .charge(-300)
-                .on("tick", tick)
-                .start();
+        var force = d3.layout.force()
+            .nodes(d3.values(nodes))
+            .links(links)
+            .size([width, height])
+            .linkDistance(100)
+            .charge(-300)
+            .on("tick", tick)
+            .start();
 
-            var svg = d3.select(".graph-chart")
-                .attr("width", width)
-                .attr("height", height);
+        var svg = d3.select(".graph-chart")
+            .attr("width", width)
+            .attr("height", height);
 
 // build the arrow.
-            svg.append("svg:defs")
-                .selectAll("marker")
-                .data(["end"])      // Different link/path types can be defined here
-                .enter().append("svg:marker")    // This section adds in the arrows
-                .attr("id", String)
-                .attr("viewBox", "0 -5 10 10")
-                .attr("refX", 15)
-                .attr("refY", -1.5)
-                .attr("markerWidth", 10)
-                .attr("markerHeight", 10)
-                .attr("orient", "auto")
-                .append("svg:path")
-                .attr("d", "M0,-5L10,0L0,5");
+        svg.append("svg:defs")
+            .selectAll("marker")
+            .data(["end"])      // Different link/path types can be defined here
+            .enter().append("svg:marker")    // This section adds in the arrows
+            .attr("id", String)
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 15)
+            .attr("refY", -1.5)
+            .attr("markerWidth", 10)
+            .attr("markerHeight", 10)
+            .attr("orient", "auto")
+            .append("svg:path")
+            .attr("d", "M0,-5L10,0L0,5");
 
 // add the links and the arrows
-            var path = svg.append("svg:g").selectAll("path")
-                .data(force.links())
-                .enter().append("svg:path")
-                //    .attr("class", function(d) { return "link " + d.type; })
-                .attr("class", "link")
-                .attr("marker-end", "url(#end)");
+        var path = svg.append("svg:g").selectAll("path")
+            .data(force.links())
+            .enter().append("svg:path")
+            //    .attr("class", function(d) { return "link " + d.type; })
+            .attr("class", "link")
+            .attr("marker-end", "url(#end)");
 
 // define the nodes
-            var node = svg.selectAll(".node")
-                .data(force.nodes())
-                .enter().append("g")
-                .attr("class", "node")
-                .call(force.drag);
+        var node = svg.selectAll(".node")
+            .data(force.nodes())
+            .enter().append("g")
+            .attr("class", "node")
+            .call(force.drag);
 
 // add the nodes
-            node.append("circle")
-                .attr("r", 10);
+        node.append("circle")
+            .attr("r", 10);
 
 // add the text
-            node.append("text")
-                .attr("x", 12)
-                .attr("dy", "2em")
-                .text(function(d) { return d.name; });
+        node.append("text")
+            .attr("x", 12)
+            .attr("dy", "2em")
+            .text(function (d) {
+                return d.name;
+            });
 
 // add the curvy lines
-            function tick() {
-                path.attr("d", function(d) {
-                    var dx = d.target.x - d.source.x,
-                        dy = d.target.y - d.source.y,
-                        dr = Math.sqrt(dx * dx + dy * dy);
-                    return "M" +
-                        d.source.x + "," +
-                        d.source.y + "A" +
-                        dr + "," + dr + " 0 0,1 " +
-                        d.target.x + "," +
-                        d.target.y;
+        function tick() {
+            path.attr("d", function (d) {
+                var dx = d.target.x - d.source.x,
+                    dy = d.target.y - d.source.y,
+                    dr = Math.sqrt(dx * dx + dy * dy);
+                return "M" +
+                    d.source.x + "," +
+                    d.source.y + "A" +
+                    dr + "," + dr + " 0 0,1 " +
+                    d.target.x + "," +
+                    d.target.y;
+            });
+
+            node
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
                 });
+        }
 
-                node
-                    .attr("transform", function(d) {
-                        return "translate(" + d.x + "," + d.y + ")"; });
-            }
-
-        });
     }
 
     render() {
