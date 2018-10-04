95841f38fe37a827ae4f3c3bb57aa443237ad952
kubarzeplinski <kuba.rzeplinski@gmail.com>
2018-10-04 18:38:45 +0200

    Graph -> sort keywords by name.

--- src/main/java/com/mgr/twitteranalyser/graph/GraphService.java
+++ src/main/java/com/mgr/twitteranalyser/graph/GraphService.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -58,6 +59,7 @@ public class GraphService {
     public List<KeywordDTO> getKeywords() {
         return keywordRepository.readAllByNameNotNull()
                 .map(KeywordDTO::new)
+                .sorted(Comparator.comparing(KeywordDTO::getName))
                 .collect(Collectors.toList());
     }
 
